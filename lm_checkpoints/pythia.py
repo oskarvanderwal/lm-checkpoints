a78c0140d3c6780945cd4e0e9049ea119bb6bfe2
Oskar van der Wal <o.d.vanderwal@gmail.com>
2024-01-25 14:21:57 +0100

    Add extra seeds now available for (some) Pythia models

--- lm_checkpoints/pythia.py
+++ lm_checkpoints/pythia.py
@@ -30,7 +30,7 @@ class PythiaCheckpoints(AbstractCheckpoints):
         if deduped:
             raise NotImplementedError
 
-        self._seeds = [0, 1, 2, 3, 4]
+        self._seeds = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
         if seed:
             assert set(seed).issubset(set(self._seeds))
             self.seeds = seed
